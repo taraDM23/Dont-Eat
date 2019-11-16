eabffb20d3b68365e42aac19a62c798c02a57007
chstrcpprpt <cjhallen@gmail.com>
2019-11-16 13:42:03 +1100

    Added AJAX calls for cuisine, location and search. searchCity is still hardcoded as "New York"

--- js/script.js
+++ js/script.js
@@ -5,20 +5,17 @@ $("#button-search").on("click", function RenderOutput(event) {
 
   // get cuisine input from HTML
   var cuisineInput = $("#input-cuisine").val();
-  console.log({cuisineInput});
 
-  // define lat & long
-  var lat = "40.742051";
-  var lon = "-74.004821";
+  // define lat & long global variables
+  var lat;
+  var lon;
 
-  // API query URL
-  var queryURL = "https://developers.zomato.com/api/v2.1/search?q=" + cuisineInput + "&count=30&lat=" + lat + "&lon=" + lon + "&radius=3&sort=rating&order=asc"
+  // API call to get city details
+  let searchCity = "New York"
+  let cityURL = `https://developers.zomato.com/api/v2.1/locations?query=${searchCity}`;
 
-  // API call to list cuisines
-  let cuisineURL = `https://developers.zomato.com/api/v2.1/cuisines?lat=${lat}&lon=${lon}`
-  
   $.ajax({
-    url: cuisineURL,
+    url: cityURL,
     method: "GET",
     headers: {
       "Accept": "application/json",
@@ -26,18 +23,62 @@ $("#button-search").on("click", function RenderOutput(event) {
     }
   })
   .then(function(response) {
-    let cuisines = response.cuisines;
-    console.log(cuisines);
 
-    // convert cuisines array into object
-    // object has cuisine_name and cuisine_id
-    let cuisinesObj = {};
-    for(let i = 0; i < cuisines.length; i++) {
-      cuisinesObj[cuisines[i].cuisine.cuisine_name] = cuisines[i].cuisine.cuisine_id;
-    };
+    let cityName = response.location_suggestions[0].city_name;
+    let cityCountry = response.location_suggestions[0].country_name;
 
-    console.log({cuisinesObj});
-  });
+    let city = (cityName + ", " + cityCountry);
+    console.log({city}); 
+
+    let lat = response.location_suggestions[0].latitude;
+    let lon = response.location_suggestions[0].longitude;
+
+    console.log(lat + " & " + lon);
+
+  
+  
+    // API call to list cuisines
+    let cuisineURL = `https://developers.zomato.com/api/v2.1/cuisines?lat=${lat}&lon=${lon}`;
+
+    $.ajax({
+      url: cuisineURL,
+      method: "GET",
+      headers: {
+        "Accept": "application/json",
+        "user-key": "911458285a16e49504124550033c5a36"
+      }
+    })
+    .then(function(response) {
+      let cuisines = response.cuisines;
+
+      // convert cuisines array into object
+      // object has cuisine_name and cuisine_id
+      let cuisinesObj = {};
+      for(let i = 0; i < cuisines.length; i++) {
+        cuisinesObj[cuisines[i].cuisine.cuisine_name] = cuisines[i].cuisine.cuisine_id;
+      };
+
+      console.log({cuisinesObj});
+    })
+
+    // run main search API
+    let cuisineId = 3;
+
+    let searchURL = `https://developers.zomato.com/api/v2.1/search?lat=${lat}&lon=${lon}&cuisines=${cuisineId}&sort=rating&order=asc`;
+  
+    $.ajax({
+      url: searchURL,
+      method: "GET",
+      headers: {
+        "Accept": "application/json",
+        "user-key": "911458285a16e49504124550033c5a36"
+      }
+    })
+    .then(function(response) {
+      console.log(response);
+      
+    })
+  
 
 
 
@@ -45,7 +86,9 @@ $("#button-search").on("click", function RenderOutput(event) {
 
 
   // AJAX API call
-  
+  // API query URL
+  // var queryURL = "https://developers.zomato.com/api/v2.1/search?q=" + cuisineInput + "&count=30&lat=" + lat + "&lon=" + lon + "&radius=3&sort=rating&order=asc"
+
   // $.ajax({
   //   url: queryURL,
   //   method: "GET",
@@ -101,4 +144,5 @@ $("#button-search").on("click", function RenderOutput(event) {
   //     $(".results-box").append(resultsDiv);
   //   }
   // });
+  })
 });
