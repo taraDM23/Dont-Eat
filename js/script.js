97afd08e0f42f95baebb4b2cea0d99c9d8f3658e
chstrcpprpt <cjhallen@gmail.com>
2019-11-16 11:12:41 +1100

    fixed formatting

--- js/script.js
+++ js/script.js
@@ -1,94 +1,92 @@
 //On click event that runs dashboard
 $("#button-search").on("click", function RenderOutput(event) {
 
-    event.preventDefault();
+  event.preventDefault();
 
-    //Zomato API key and URL
-    var key = "911458285a16e49504124550033c5a36";
+  //Zomato API key and URL
+  var key = "911458285a16e49504124550033c5a36";
 
-    //location and cuisine input
-    var cuisineInput = $("#input-cuisine").val();
-    //var location = $("#input-city").val();
+  //location and cuisine input
+  var cuisineInput = $("#input-cuisine").val();
+  //var location = $("#input-city").val();
 
 
-    /* //Location key and URL
-        var APIKey = "";
-        var queryURL = """;
+  /* //Location key and URL
+  var APIKey = "";
+  var queryURL = """;
+  //JS code that will get the current location
+  $.ajax({
+  url: queryURL,
+  method: "GET"
+  })
+  .then(function(response) {
+    console.log(response);
+  //Get Lat and Lon for next query
+  var lat = response.coord.lat;
+  console.log(lat);
+  var lon = response.coord.lon;
+  console.log(lon); 
+  */
 
-        //JS code that will get the current location
-          $.ajax({
-          url: queryURL,
-          method: "GET"
-          })
+  // static coord for now
 
-          .then(function(response) {
-           console.log(response);
-        //Get Lat and Lon for next query
+  var lat = "-33.6";
+  var lon = "150.75";
+  var queryURL = "https://developers.zomato.com/api/v2.1/search?q=" + cuisineInput + "&count=30&lat=" + lat + "&lon=" + lon + "&radius=3&sort=rating&order=asc"
 
-            var lat = response.coord.lat;
-            console.log(lat);
-            var lon = response.coord.lon;
-            console.log(lon); 
-     */
-    // static coord for now
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    headers: {
+      "Accept": "application/json",
+      "user-key": "911458285a16e49504124550033c5a36"
+    }
+  })
+  .then(function(response) {
+    console.log(response);
+    var resList = response.restaurants;
+    console.log(resList)
 
-    var lat = "-33.6";
-    var lon = "150.75";
-    var queryURL = "https://developers.zomato.com/api/v2.1/search?q=" + cuisineInput + "&count=30&lat=" + lat + "&lon=" + lon + "&radius=3&sort=rating&order=asc"
+    for (i = 0; 1 < resList.length; i++) {
+      //Div to store results  
+      var resultsDiv = $("<div>");
+      resultsDiv.addClass("resultDisplay")
 
-    $.ajax({
-            url: queryURL,
-            method: "GET",
-            headers: {
-                "Accept": "application/json",
-                "user-key": "911458285a16e49504124550033c5a36"
-            }
-        })
-        .then(function(response) {
-            console.log(response);
-            var resList = response.restaurants;
-            console.log(resList)
+      // GET restaurant name
+      var resName = resList[i].restaurant.name;
+      console.log(resName)
 
-            for (i = 0; 1 < resList.length; i++) {
-                //Div to store results  
-                var resultsDiv = $("<div>");
-                resultsDiv.addClass("resultDisplay")
+      //creating element
+      var Name = $("<h4>").text(resName);
 
-                // GET restaurant name
-                var resName = resList[i].restaurant.name;
-                console.log(resName)
+      //display element
+      resultsDiv.append(Name);
 
-                //creating element
-                var Name = $("<h4>").text(resName);
+      // GET restaurant rating
+      var resRate = resList[i].restaurant.user_rating.aggregate_rating;
+      console.log(resRate)
 
-                //display element
-                resultsDiv.append(Name);
+      // GET restaurant vote
+      var resVote = resList[i].restaurant.user_rating.votes;
+      console.log(resVote)
 
-                // GET restaurant rating
-                var resRate = resList[i].restaurant.user_rating.aggregate_rating;
-                console.log(resRate)
+      //creating element
+      var Rating = $("<p>").text(resRate + " from " + resVote + " votes.");
 
-                // GET restaurant vote
-                var resVote = resList[i].restaurant.user_rating.votes;
-                console.log(resVote)
+      //display element
+      resultsDiv.append(Rating);
 
-                //creating element
-                var Rating = $("<p>").text(resRate + " from " + resVote + " votes.");
+      // GET restaurant address
+      var resAddress = resList[i].restaurant.location.address;
+      console.log(resAddress)
 
-                //display element
-                resultsDiv.append(Rating);
+      //creating element
+      var Address = $("<p>").text(resAddress);
 
-                // GET restaurant address
-                var resAddress = resList[i].restaurant.location.address;
-                console.log(resAddress)
+      //display element
+      resultsDiv.append(Address);
 
-                //creating element
-                var Address = $("<p>").text(resAddress);
-
-                //display element
-                resultsDiv.append(Address);
-
-                $(".results-box").append(resultsDiv);
-            }
-        });
+      $(".results-box").append(resultsDiv);
+    }
+  });
 })
