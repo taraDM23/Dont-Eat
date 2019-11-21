2ea36566c5a8a05bf01d99ff13f258410e312108
chstrcpprpt <cjhallen@gmail.com>
2019-11-21 16:22:21 +1100

    formatted Keegans spacing

--- js/script.js
+++ js/script.js
@@ -203,8 +203,8 @@ function initMap() {
   function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
-                      'Error: The Geolocation service failed.' :
-                      'Error: Your browser doesn\'t support geolocation.');
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
     infoWindow.open(map);
   };
 
